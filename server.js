22b5947787941df60873f8fcaeacb8483f57a7de
damodaranj <40349259+damodaranj@users.noreply.github.com>
2022-09-22 13:06:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - 2 build via gitactions')
+  res.send('Hello World! - welcome to greens')
 })
 
 app.listen(port, () => {
