d4baee183d68d7410c4489258c1ba0f8e8845b11
damodaranj <40349259+damodaranj@users.noreply.github.com>
2022-10-13 18:28:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - welcome to greens')
+  res.send('Hello World! - welcome to greensss')
 })
 
 app.listen(port, () => {
